1c4cfc4e0d12acd9266729137d2f15bcbb30c278
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-07-17 23:34:00 -0500

    mvp complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import {Image} from "./Image.js"
+const axios = require('axios');
+
+let nasaAPI = 'https://api.nasa.gov/planetary/apod?api_key=r2F92MiuirP5Fveu0JZ8uc6kD5yRGVaSlVJraSTQ&date=2012-03-14';
+
 
 function App() {
+  let [API, setAPI] = useState(nasaAPI);
+  let [content, setContent] = useState('');
+  useEffect(() => {
+    axios.get(API)
+    .then(res => {
+      setContent(res);
+    })
+    .catch(e => {
+      console.log(e)
+    });
+  }, [API]);
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      {content !== ''? <Image imgUrl={content.data.url} title={content.data.title} date={content.data.date} content={content.data.explanation}/>: console.log("Loading...") }
     </div>
   );
 }
---
+++ src/Image.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Image = props => {
+  
+  return (
+    <>
+    <h1>{props.title}</h1>
+    <h2>{props.date}</h2>
+    <img src={props.imgUrl}></img>
+    <p>{props.content}</p>
+      { /* Display a button element rendering the data being passed down from the parent container on props */}
+    </>
+  );
+};
